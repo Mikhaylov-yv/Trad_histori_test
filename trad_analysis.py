968858d04b0cadce7a7edba76bec8b8931ad007d
dezmond22 <dezmond22@yandex.ru>
2020-04-30 01:05:41 +0300

    Отделил разметку от тестирования

--- trad_analysis.py
+++ trad_analysis.py
@@ -21,15 +21,51 @@ class Analysis:
     #                                          инвистиционная('investment')
     # signal(str) - тип принимаемых сигналов для торговли: пересечение 2-х скользящих средних ('two_ma_samp')
 
-    def get_test(self, money = 1000000,strategy_trad = 'position', signal= 'two_ma_samp',
-                 signal_param = {'ma_samp_slow_param' : 350,
-                          'ma_samp_fast_param' : 100,
-                          'pct_change' : 0.01}):
-        # Разметка данных по выбранному типу сигналов
-        if signal == 'two_ma_samp': df_dikt = self._get_resempl(self._get_two_ma_samp(signal_param))
-        else: df_dikt = self._get_resempl(self._get_two_ma_samp()) # На время пока нет других стратегий
+    # Расчет 2-х скользящих средних и среднего процентного изменения самой быстрой
+    def get_two_ma_samp(self):
+        param = {'ma_samp_slow_param': 350,
+                        'ma_samp_fast_param': 100,
+                        'pct_change': 0.01}
+        df_dikt = self.df_dikt
+        for df_toll in list(self.df_dikt):
+            df_dikt[df_toll]['ma_samp_slow'] = df_dikt[df_toll].CLOSE.rolling(window=param['ma_samp_slow_param'],
+                                                                              min_periods=param[
+                                                                                  'ma_samp_slow_param']).median()
+            df_dikt[df_toll]['ma_samp_fast'] = df_dikt[df_toll].CLOSE.rolling(window=param['ma_samp_fast_param'],
+                                                                              min_periods=param[
+                                                                                  'ma_samp_fast_param']).median()
+            df_dikt[df_toll]['ma_samp_fast_pct_change'] = df_dikt[df_toll]['ma_samp_fast'].pct_change(
+                periods=param['ma_samp_fast_param'])
+        self.df_cignal_dict = self._get_intersection_two_ma_samp(df_dikt)
+        return self
+
+        # Установка сигналов
+
+    def _get_intersection_two_ma_samp(self, df_dikt):
+
+        i = 2
+        for df_toll in list(df_dikt):
+            ma_difference = []
+            for index in range(len(df_dikt[df_toll].index)):
+                ma_difference.append(df_dikt[df_toll].loc[df_dikt[df_toll].index[index],
+                                                          'ma_samp_slow'] - df_dikt[df_toll].loc[
+                                         df_dikt[df_toll].index[index], 'ma_samp_fast'])
+                if index < i: continue
+                ma_samp_fast_pct_change = df_dikt[df_toll].loc[df_dikt[df_toll].index[index], 'ma_samp_fast_pct_change']
+                ma_sum = ma_difference[-1] + ma_difference[-1 - i]
+                peresech = abs(ma_sum) < abs(ma_difference[-1]) and abs(ma_sum) < abs(
+                    ma_difference[-1 - i])  # nd abs(ma_samp_fast_pct_change) > pct_change
+                #         peresech = abs(ma_samp_fast_pct_change) > 0.01 # 1%
+                #     if peresech == True: print(df_test.index[index])
+                df_dikt[df_toll].loc[df_dikt[df_toll].index[index], 'SIGNAL'] = peresech
+                df_dikt[df_toll].loc[df_dikt[df_toll].index[index],
+                                     'POZISION'] = 'short' if ma_samp_fast_pct_change > 0 else 'long'
+        return df_dikt
+
+    def get_test(self, money = 100000,strategy_trad = 'position'):
         # Получение размеченного df с сигналами для сделок
-        self.df_cignal_dict = df_dikt
+        if 'POZISION' not in list(self.df_cignal_dict[list(self.df_cignal_dict)[0]]): print('В данных нет разметки')
+        df_dikt = self._get_resempl(self.df_cignal_dict)
         self.portfel_df_dict = {}
         # Инициализация словаря с актуальными котировками на каждый цикл
         cot_dict = self._get_cot_dict()
@@ -79,6 +115,10 @@ class Analysis:
                     self.portfel_df_dict[tool].loc[self.data_period_dick['end'], 'Portfel_vol']))
         return self
 
+
+    def rept(self, type):
+        self.portfel_df_dict
+
     # Определение величины количества лотов для покпки 1/4 цены портфеля
     def _get_lot_size(self, val_portfel, lot_price, lot_size):
         lpoz_size = 1 / 4
@@ -100,39 +140,6 @@ class Analysis:
         data_period_dick['step'] = data_period_dick['step'][0]
         return data_period_dick
 
-    # Расчет 2-х скользящих средних и среднего процентного изменения самой быстрой
-    def _get_two_ma_samp(self, param):
-        df_dikt = self.df_dikt
-        for df_toll in list(self.df_dikt):
-            df_dikt[df_toll]['ma_samp_slow'] = df_dikt[df_toll].CLOSE.rolling(window=param['ma_samp_slow_param'],
-                                                                              min_periods=param['ma_samp_slow_param']).median()
-            df_dikt[df_toll]['ma_samp_fast'] = df_dikt[df_toll].CLOSE.rolling(window=param['ma_samp_fast_param'],
-                                                                              min_periods=param['ma_samp_fast_param']).median()
-            df_dikt[df_toll]['ma_samp_fast_pct_change'] = df_dikt[df_toll]['ma_samp_fast'].pct_change(
-                periods=param['ma_samp_fast_param'])
-        df_dikt = self._get_intersection_two_ma_samp(df_dikt)
-        return df_dikt
-
-    # Установка сигналов
-    def _get_intersection_two_ma_samp(self, df_dikt):
-
-        i = 2
-        for df_toll in list(df_dikt):
-            ma_difference = []
-            for index in range(len(df_dikt[df_toll].index)):
-                ma_difference.append(df_dikt[df_toll].loc[df_dikt[df_toll].index[index],
-                        'ma_samp_slow'] - df_dikt[df_toll].loc[df_dikt[df_toll].index[index], 'ma_samp_fast'])
-                if index < i: continue
-                ma_samp_fast_pct_change = df_dikt[df_toll].loc[df_dikt[df_toll].index[index], 'ma_samp_fast_pct_change']
-                ma_sum = ma_difference[-1] + ma_difference[-1 - i]
-                peresech = abs(ma_sum) < abs(ma_difference[-1]) and abs(ma_sum) < abs(
-                    ma_difference[-1 - i])  # nd abs(ma_samp_fast_pct_change) > pct_change
-                #         peresech = abs(ma_samp_fast_pct_change) > 0.01 # 1%
-                #     if peresech == True: print(df_test.index[index])
-                df_dikt[df_toll].loc[df_dikt[df_toll].index[index], 'SIGNAL'] = peresech
-                df_dikt[df_toll].loc[df_dikt[df_toll].index[index],
-                                     'POZISION'] = 'short' if ma_samp_fast_pct_change > 0 else 'long'
-        return df_dikt
 
     # Создание периода для стороннего использования
     # хорошо бы убрать
@@ -170,8 +177,9 @@ if __name__ == '__main__':
         #     df = df.loc[df.index <= '2019-07-10']
         df_dikt[toll_path.split('_')[0]] = df
 
-    t = Analysis(df_dikt).get_test()
-    t
+    t = Analysis(df_dikt).get_two_ma_samp()
+    print(t.df_cignal_dict['HYDR'])
+    t.get_test()
 
 
 
